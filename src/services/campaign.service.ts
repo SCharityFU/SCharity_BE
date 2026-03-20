4502b1bc289eb6375d91defd73d5c68a021c5dea
HUYEN CAO <huyenctt.dev@gmail.com>
2026-03-20 22:30:12 +0700

    fix(cache): update cache when campaign was updated

--- src/services/campaign.service.ts
+++ src/services/campaign.service.ts
@@ -235,8 +235,8 @@ export class CampaignService {
   async getCampaignById(id: string) {
     // For now this one is not cached so that user can see the latest data immediately after donation
     const cacheKey = `campaign:v2:${id}`;
-    // const cached = await redisClient.get(cacheKey);
-    // if (cached) return JSON.parse(cached);
+    const cached = await redisClient.get(cacheKey);
+    if (cached) return JSON.parse(cached);
 
     const campaign = await CampaignRepository.findOne({
       where: { id },
@@ -279,7 +279,7 @@ export class CampaignService {
 
     const mapped = mapCampaignDetailDto(campaign);
 
-    await redisClient.setex(cacheKey, CAMPAIGN_CACHE_TTL, JSON.stringify(mapped));
+    // await redisClient.setex(cacheKey, CAMPAIGN_CACHE_TTL, JSON.stringify(mapped));
     return mapped;
   }
 
